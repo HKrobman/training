756a89bc56075f1511bc29ab9a9fa056406e15b7
HKrobman <32384523+HKrobman@users.noreply.github.com>
2019-01-19 19:44:41 +0900

    Update Piramid.java

--- Piramid.java
+++ Piramid.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 public class Piramid {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		sc.close();
-
 		System.out.println("n段のピラミッドを表示します");
 		System.out.print("段数は：");
 		int count = sc.nextInt();
+		sc.close();
 
 		for (int i = 1; i <= count; i++) {//段
 			for (int j = 1; j < count + i ; j++) {//行
