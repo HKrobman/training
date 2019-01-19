c775c70101f4e66bc0080aa271cbda29e77be619
github HKrobman <output_sa@outlook.jp>
2019-01-19 18:07:50 +0900

    build Piramid

---
+++ Piramid.java
@@ -0,0 +1,26 @@
+package training;
+import java.util.Scanner;
+
+public class Piramid {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+
+		System.out.println("n段のピラミッドを表示します");
+		System.out.print("段数は：");
+		int count = sc.nextInt();
+
+		for (int i = 1; i <= count; i++) {//段
+			for (int j = 1; j < count + i ; j++) {//行
+				if (j <= count - i) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+	int a = Integer.parseInt("2");
+}
