cb21f05ca2516fa558730dd5bc7b01629c952173
ha_kido <harutaka814@gmail.com>
2019-02-03 23:02:16 +0900

    リファクタリング

--- Byouin.java
+++ Byouin.java
@@ -3,14 +3,14 @@ package training;
 import java.util.Scanner;
 public class Byouin {
 	public static void main(String[] args) {
-		String youbi = "";
+		String goToHospitalDay = "";
 		System.out.println("病院に行く曜日を入力してください");
 		System.out.println("日曜, 月曜, 火曜, 水曜, 木曜, 金曜, 土曜");
 		Scanner scanner = new java.util.Scanner(System.in);
-		youbi = scanner.nextLine();
+		goToHospitalDay = scanner.nextLine();
 		scanner.close();
 
-		switch(youbi) {
+		switch(goToHospitalDay) {
 		case "月曜":
 		case "火曜":
 		case "水曜":
--- Candy.java
+++ Candy.java
@@ -14,13 +14,15 @@ public class Candy {
 		*/
 		System.out.println("子供2人にあげるキャンディーの個数を入力してください");
 		Scanner scanner = new java.util.Scanner(System.in);
-		int num = scanner.nextInt();
+		int numberOfCandy = scanner.nextInt();
 		scanner.close();
+		int candyPerPeson = numberOfCandy/2;
+		int excessiveCandy = numberOfCandy%2;
 		System.out.print("一人がもらえるキャンディーの個数は");
-		if(num%2==0) {
-			System.out.println(num/2 + "個");
+		if(excessiveCandy == 0) {
+			System.out.println(candyPerPeson + "個");
 		}else {
-			System.out.println(num/2 + "個あまり" + num%2);
+			System.out.println(candyPerPeson + "個あまり" + excessiveCandy);
 		}
 	}
 }
