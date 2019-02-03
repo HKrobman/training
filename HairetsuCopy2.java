116a71feca0e6065035784a206f6bae560f5b2aa
HKrobman <32384523+HKrobman@users.noreply.github.com>
2019-02-04 00:17:42 +0900

    Create HairetsuCopy2.java

---
+++ HairetsuCopy2.java
@@ -0,0 +1,18 @@
+package training;
+
+public class HairetsuCopy2 {
+	public static void main(String[] args){
+	//do whileを使った場合との違いを確認
+		  int[] data = {54, 17, 27, 38, 45, -1, 12, 8};
+		  int[] copy = new int[data.length];
+		  int i = 0;
+		  do{
+		  	copy[i] = data[i];
+		  }while(data[i++] != -1);
+
+		  for(i = 0; i < copy.length; i++){
+		  	System.out.println(copy[i]);
+		  }
+
+	}
+}
